02ec9054e363e5df563e4a117c69e95a8f6e0353
klhaug <kristian@snougle.no>
2025-04-24 16:57:20 +0200

    working on making the toggle function work. Mobile layout somewhat done

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
+const shareIconContainer = document.getElementById("share-icon-container");
 const shareIcon = document.getElementById("share-icon");
+const textSection = document.querySelector(".text-container");
 
-shareIcon.addEventListener("onclick", () => {
-    console.log("it's working");
+shareIconContainer.addEventListener("mouseenter", () => {
+    shareIconContainer.style.backgroundColor = "#6E8098";
+    shareIcon.firstChild.setAttribute("fill", "#ffffff")
+    
+    fill.innerHTML = "#ffffff"
+    shareIcon.style.color = "lightgrey"
+})
+shareIconContainer.addEventListener("mouseleave", () => {
+    shareIconContainer.style.backgroundColor = "#ECF2F8";
+    shareIcon.firstChild.setAttribute("fill", "#6E8098")
 })
